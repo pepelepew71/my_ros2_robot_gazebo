1a9971464bc65ab0d5726d88da00ff9808825822
yc.huang71 <yc.huang71@gmail.com>
2021-10-14 19:16:41 +0800

    add some world files

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+import os
+from glob import glob
 from setuptools import setup
 
 package_name = 'my_ros2_robot_gazebo'
@@ -7,9 +9,10 @@ setup(
     version='0.0.0',
     packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
+        (os.path.join('share', package_name, 'worlds'), glob('worlds/*.world')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
---
+++ worlds/README.md
@@ -0,0 +1,4 @@
+# worlds
+
+- empty.world
+    - copy from `/opt/ros/galactic/share/turtlebot3_gazebo/worlds/empty_world.world`
